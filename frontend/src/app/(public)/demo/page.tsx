ea72796dee7dabcb6406e78bf22715707bafe610
DZykov <dzykov@protonmail.com>
2023-09-26 23:19:10 -0400

    add editor functionality: headers, bold, italic, striker, underline, list order, list bullet, allign left/center/right, font size, font name, color font, color hilite, code, quote

--- frontend/src/app/(public)/demo/page.tsx
+++ frontend/src/app/(public)/demo/page.tsx
@@ -2,84 +2,98 @@
 
 import sanitizeHtml from "sanitize-html"
 import ContentEditable from 'react-contenteditable';
-import { createRef, useCallback, useState } from "react";
+import Script from 'next/script'
+import { createRef, useCallback, useEffect, useRef, useState } from "react";
 
 
 export default function Demo() {
 
-    const [content, setContent] = useState("");
+    const [content, setContent] = useState("<br><br><br><br>"); // start with empty lines => don't lock in blocks
     const [fontColour, setFontColour] = useState("#000000");
     const [highlightColour, setHighLightColour] = useState("#ffffff");
-    const [fontSizeEditor, setFontSize] = useState("Medium");
-    const [fontStyleEditor, setFontStyle] = useState("Times New Roman");
+    const [fontSize, setFontSize] = useState("Medium");
+    const [fontName, setFontName] = useState("sans-serif");
 
-
-    const contentArea = createRef<HTMLElement>();
-
+    // const contentArea = createRef<HTMLElement>();
     const fontSizeSymbol = "12px";
     const heightSymbol = "15px";
 
+    useEffect(() => {
+        initEditor();
+    }, []);
+
+
+    const initEditor = () => {
+        cmdFunc("fontSize", false, "3");
+        cmdFunc("fontName", false, "sans-serif");
+    }
 
     const onContentChange = (evt: any) => {
         const sanitizeConf = {
-            allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3"],
-            allowedAttributes: { a: ["href"] }
+            allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3", "strong",
+                "div", "br", "u", "strike", "ul", "li", "ol", "font", "blockquote"],
+            allowedAttributes: { a: ["href", "face", "size", "color", "style", "align"] }
         };
-        console.log()
-        setContent(sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf))
+        console.log(content)
+        // console.log(document.queryCommandSupported("fontName"))
+        setContent(evt.currentTarget.innerHTML)
     }
 
     function ControlPanel() {
         return (
             <>
                 <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="bold" ui={false} arg=""
                         icon={<div className="font-bold" style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"B"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="italic" ui={false} arg=""
                         icon={<div className="italic" style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"I"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="underline" ui={false} arg=""
                         icon={<div className="underline-offset-1" style={{ fontSize: fontSizeSymbol, height: heightSymbol, textDecoration: "underline" }}>{"U"}</div>}
+                    />
+
+                    <CmdButton cmd="strikeThrough" ui={false} arg=""
+                        icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol, }}>{<><s>S</s></>}</div>}
                     />
                 </span>
 
                 <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="insertUnorderedList" ui={false} arg=""
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"•"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="insertOrderedList" ui={false} arg=""
                         icon={<div className="underline-offset-1" style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"½"}</div>}
                     />
                 </span>
 
                 <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="justifyLeft" ui={false} arg=""
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"|="}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="justifyCenter" ui={false} arg=""
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"|=|"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="justifyRight" ui={false} arg=""
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"=|"}</div>}
                     />
                 </span>
 
                 <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="formatBlock" ui={false} arg="<H1>"
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"H1"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="formatBlock" ui={false} arg="<H2>"
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"H2"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="formatBlock" ui={false} arg="<H3>"
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"H3"}</div>}
                     />
                 </span>
@@ -93,10 +107,10 @@ export default function Demo() {
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"Link"}</div>}
                     />
 
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="insertHTML" ui={false} arg="<pre><code>"
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"Code"}</div>}
                     />
-                    <CmdButton cmd="copy" ui={false} arg=""
+                    <CmdButton cmd="formatBlock" ui={false} arg="<blockquote>"
                         icon={<div style={{ fontSize: fontSizeSymbol, height: heightSymbol }}>{"Quote"}</div>}
                     />
                 </span>
@@ -107,27 +121,39 @@ export default function Demo() {
                     <select
                         name="FontSize"
                         id="FontSize"
+                        value={fontSize}
                         className="inline-flex p-1 overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2"
-                        onChange={e => setFontSize(e.target.value)}
+                        onChange={function (e) {
+                            e.preventDefault();
+                            cmdFunc("fontSize", false, e.target.value);
+                            setFontSize(e.target.value);
+                        }}
                     >
-                        <option value="Medium">Medium</option>
-                        <option value="Tiny">Tiny</option>
-                        <option value="Small">Small</option>
-                        <option value="Large">Large</option>
-                        <option value="X-Large">X-Large</option>
+                        <option value="3">Medium</option>
+                        <option value="1">Tiny</option>
+                        <option value="2">Small</option>
+                        <option value="4">Large</option>
+                        <option value="5">X-Large</option>
                     </select>
 
                     <select
                         name="FontStyle"
                         id="FontStyle"
+                        value={fontName}
                         className="inline-flex p-1 overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2"
-                        onChange={e => setFontStyle(e.target.value)}
+                        onChange={function (e) {
+                            e.preventDefault();
+                            cmdFunc("fontName", false, e.target.value);
+                            setFontName(e.target.value);
+                        }}
                     >
+                        <option value="sans-serif">sans-serif</option>
                         <option value="Times New Roman">Times New Roman</option>
                         <option value="Calibri">Calibri</option>
-                        <option value="Ubuntu Mono">Ubuntu Mono</option>
-                        <option value="Openic">Openic</option>
-                        <option value="Fonty Beauty">Fonty Beauty</option>
+                        <option value="Arial">Arial</option>
+                        <option value="Verdana">Verdana</option>
+                        <option value="Courier New">Courier New</option>
+                        <option value="Cursive">Cursive</option>
                     </select>
 
                 </div>
@@ -136,13 +162,21 @@ export default function Demo() {
 
                 <div className="max-h-[25px] inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
                     <input value={fontColour} id="colorPickerFont" type="color"
-                        onChange={e => setFontColour(e.target.value)} />
+                        onChange={function (e) {
+                            e.preventDefault();
+                            cmdFunc("foreColor", false, e.target.value);
+                            setFontColour(e.target.value);
+                        }} />
                     &nbsp;Font Colour&nbsp;
                 </div>
 
                 <div className="max-h-[25px] inline-flex overflow-hidden rounded-md border bg-white shadow-sm my-2 mr-2">
                     <input value={highlightColour} id="colorPickerHighlight" type="color"
-                        onChange={e => setHighLightColour(e.target.value)} />
+                        onChange={function (e) {
+                            e.preventDefault();
+                            cmdFunc("hiliteColor", false, e.target.value);
+                            setHighLightColour(e.target.value);
+                        }} />
                     &nbsp;Highlight Colour&nbsp;
                 </div>
             </>
@@ -155,14 +189,19 @@ export default function Demo() {
                 className="inline-block p-2 text-gray-700 hover:bg-gray-50 focus:relative"
                 key={props.cmd}
                 onMouseDown={(evt) => {
-                    evt.preventDefault(); // Avoids loosing focus from the editable area
-                    document.execCommand(props.cmd, false, props.arg); // Send the command to the browser
+                    evt.preventDefault();
+                    document.execCommand(props.cmd, props.ui, props.arg);
                 }}
             >
                 {props.icon}
             </button>
         );
     }
+
+    function cmdFunc(cmd: string, ui: boolean, arg: string) {
+        document.execCommand(cmd, ui, arg);
+    }
+
 
     return (
         <>
@@ -172,10 +211,10 @@ export default function Demo() {
                 onChange={onContentChange}
                 onBlur={onContentChange}
                 html={content}
-                innerRef={contentArea}
+                // innerRef={contentArea}
                 disabled={false}
                 style={{ whiteSpace: "pre-wrap", overflowWrap: "break-word", minHeight: "500px", maxWidth: "900px" }}
-                className="block p-2.5 text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                className="block py-3 px-5 text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
             />
         </>
     )
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from 'tailwindcss'
+const plugin = require('tailwindcss/plugin')
+
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  blocklist: [
+
   ],
   theme: {
     extend: {
@@ -15,6 +20,14 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }: any) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl'), fontWeight: theme('fontWeight.bold') },
+        'h2': { fontSize: theme('fontSize.xl'), fontWeight: theme('fontWeight.bold') },
+        'h3': { fontSize: theme('fontSize.lg'), fontWeight: theme('fontWeight.bold') },
+      })
+    })
+  ],
 }
 export default config
